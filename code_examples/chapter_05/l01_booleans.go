f5d692a511525ea141e70d33e7adc3b9888cf4a6
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-03-09 10:31:07 +0800

    Added notes

--- code_examples/chapter_05/l01_booleans.go
+++ code_examples/chapter_05/l01_booleans.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	bool1 := true
+	// While ( ) around the conditions are not needed, for complex conditions they may be used to make the code clearer.
 	if bool1 {
 		fmt.Printf("The value is true\n")
 	} else {
