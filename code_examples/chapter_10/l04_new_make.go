9a6f5889a36d9da2a295f77b5cd2dce6861f89c7
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-03-13 17:30:24 +0800

    Added notes

--- code_examples/chapter_10/l04_new_make.go
+++ code_examples/chapter_10/l04_new_make.go
@@ -2,6 +2,8 @@
 package main
 
 type Foo map[string]string
+
+// 结构体
 type Bar struct {
 	thingOne string
 	thingTwo int
@@ -9,10 +11,13 @@ type Bar struct {
 
 func main() {
 	// OK:
+	// 可以对 struct new
 	y := new(Bar)
 	(*y).thingOne = "hello"
 	(*y).thingTwo = 1
+	
 	// not OK:
+	// 不能对 struct 进行 make
 	z := make(Bar) // compile error: cannot make type Bar
 	z.thingOne = "hello"
 	z.thingTwo = 1
@@ -21,6 +26,7 @@ func main() {
 	x := make(Foo)
 	x["x"] = "goodbye"
 	x["y"] = "world"
+
 	// not OK:
 	u := new(Foo)
 	(*u)["x"] = "goodbye" // !! panic !!: runtime error: assignment to entry in nil map
